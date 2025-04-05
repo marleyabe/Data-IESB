cc59f42c349e85cc2281ec9d1b4ef7b23cd2b369
Marley Abe <marleyabe@gmail.com>
2025-04-05 10:50:13 -0300

    Update Cors

--- backend/app.py
+++ backend/app.py
@@ -17,7 +17,7 @@ from relatorios import relatorios_bp
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)
+    CORS(app, supports_credentials=True, resources={r"/api/*": {"origins": "*"}})
 
     app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "chave_padrao")
     
